3d52d1f9edba6661bc2e174a9aa44cd14797b65a
CoolePizza <coolepizza@gmx.de>
2021-05-18 21:41:17 +0200

    Set mongo uri to an env variable

--- scr/utils/DatabaseHelper.ts
+++ scr/utils/DatabaseHelper.ts
@@ -3,7 +3,7 @@ import * as mongoose from "mongoose";
 export default class DatabaseHelper {
 
     static connect(){
-        mongoose.connect('mongodb://localhost/pizza-bot',{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+        mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
             console.log("Connected to MongoDB")
         })
     }
