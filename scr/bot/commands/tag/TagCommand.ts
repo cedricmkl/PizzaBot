ef7b763b7448cd429bef049e2552244158a04877
CoolePizza <coolepizza@gmx.de>
2021-05-18 21:49:26 +0200

    Tag Command fix

--- scr/bot/commands/tag/TagCommand.ts
+++ scr/bot/commands/tag/TagCommand.ts
@@ -8,12 +8,10 @@ export default class TagCommand extends Command {
     constructor() {
         super("tag", "Einen Tag in den Channel senden");
         this.withParameter("name", "Name des Tags", CommandParameterType.STRING, true)
-        this.withParameter("iwas", "iwas/ des Tags", CommandParameterType.INTEGER, false)
 
     }
 
     execute(client: Client, member: GuildMember, args: CommandArguments) {
         console.log(args.getArgument("name").getAsString())
-        console.log(args.getArgument("iwas").getAsInt())
     }
 }
