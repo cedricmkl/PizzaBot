4753a0ee2467071c2eebd41621618abba643971a
CoolePizza <hello@coolepizza.de>
2021-09-15 07:19:49 +0200

    Fix database

--- src/utils/DatabaseHelper.ts
+++ src/utils/DatabaseHelper.ts
@@ -2,9 +2,13 @@ import * as mongoose from "mongoose";
 
 export default class DatabaseHelper {
 
-    static connect() {
-        mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-            console.log("Connected to MongoDB")
-        })
+    static async connect() {
+        try {
+            console.log("Trying to connect to mongodb")
+            await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+            console.log("Connected to mongodb")
+        }catch (e) {
+            console.error(e)
+        }
     }
 }
